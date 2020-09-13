ad912c37ed1f70fb96bbbb3b2af4c46d86e285e1
Oskar Rek <oskarrek98@gmail.com>
2020-09-14 01:07:17 +0200

    refactor: create extentions for splash activity

--- app/src/main/java/dog/snow/androidrecruittest/ui/page/splash/SplashActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/ui/page/splash/SplashActivity.kt
@@ -1,29 +1,22 @@
 package dog.snow.androidrecruittest.ui.page.splash
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.android.AndroidInjection
 import dog.snow.androidrecruittest.R
 import dog.snow.androidrecruittest.data.source.remote.Resource
 import dog.snow.androidrecruittest.databinding.SplashActivityBinding
 import dog.snow.androidrecruittest.ui.base.BaseActivity
-import dog.snow.androidrecruittest.ui.common.view_model.ViewModelFactory
 import dog.snow.androidrecruittest.ui.page.main.MainActivity
+import dog.snow.androidrecruittest.utils.setLoadingView
+import dog.snow.androidrecruittest.utils.showError
 import dog.snow.androidrecruittest.utils.subscribe
-import okhttp3.Cache
-import okhttp3.OkHttpClient
-import java.io.File
-import javax.inject.Inject
+
 
 class SplashActivity : BaseActivity() {
-    private lateinit var splashViewModel: SplashViewModel
-    private lateinit var binding: SplashActivityBinding
+    lateinit var splashViewModel: SplashViewModel
+    lateinit var binding: SplashActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -38,26 +31,11 @@ class SplashActivity : BaseActivity() {
         splashViewModel = ViewModelProvider(this, viewModelFactory)[SplashViewModel::class.java]
         splashViewModel.fetchDataState.subscribe(this) {
             setLoadingView(false)
-            when(it) {
+            when (it) {
                 is Resource.Success -> redirectToActivity(MainActivity::class.java)
                 is Resource.Loading -> setLoadingView(true)
-                is Resource.Error   -> showError(it.message)
+                is Resource.Error -> showError(it.message)
             }
         }
-    }
-
-    private fun setLoadingView(isLoading: Boolean) {
-        binding.layoutProgressbar.progressbar.visibility = if(isLoading) View.VISIBLE else View.GONE
-    }
-
-    private fun showError(errorMessage: String?) {
-        MaterialAlertDialogBuilder(this)
-            .setTitle(R.string.cant_download_dialog_title)
-            .setMessage(getString(R.string.cant_download_dialog_message, errorMessage))
-            .setPositiveButton(R.string.cant_download_dialog_btn_positive) { _, _ -> splashViewModel.fetchData() }
-            .setNegativeButton(R.string.cant_download_dialog_btn_negative) { _, _ -> finish() }
-            .create()
-            .apply { setCanceledOnTouchOutside(false) }
-            .show()
     }
 }
