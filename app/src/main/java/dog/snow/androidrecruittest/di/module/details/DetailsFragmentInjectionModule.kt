15cb18d89441a3edd20c3aad3330b99e708da589
Oskar Rek <oskarrek98@gmail.com>
2020-09-14 18:50:16 +0200

    feat: navigate to details

--- app/src/main/java/dog/snow/androidrecruittest/di/module/details/DetailsFragmentInjectionModule.kt
+++ app/src/main/java/dog/snow/androidrecruittest/di/module/details/DetailsFragmentInjectionModule.kt
@@ -4,12 +4,9 @@ import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import dagger.Module
 import dagger.Provides
-import dog.snow.androidrecruittest.di.module.list.ListFragmentInjectionModule
 import dog.snow.androidrecruittest.ui.common.view_model.ViewModelFactory
 import dog.snow.androidrecruittest.ui.page.details.DetailsFragment
 import dog.snow.androidrecruittest.ui.page.details.DetailsViewModel
-import dog.snow.androidrecruittest.ui.page.list.ListFragment
-import dog.snow.androidrecruittest.ui.page.list.ListViewModel
 
 @Module
 class DetailsFragmentInjectionModule {
