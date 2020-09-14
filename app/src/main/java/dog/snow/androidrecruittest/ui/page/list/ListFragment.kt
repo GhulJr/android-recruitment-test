b610d21465795036c25d3d5a49ed72a35da33a72
Oskar Rek <oskarrek98@gmail.com>
2020-09-14 13:20:29 +0200

    fix: bind my list fragment

--- app/src/main/java/dog/snow/androidrecruittest/ui/page/list/ListFragment.kt
+++ app/src/main/java/dog/snow/androidrecruittest/ui/page/list/ListFragment.kt
@@ -1,5 +1,6 @@
 package dog.snow.androidrecruittest.ui.page.list
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,15 +8,20 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import dagger.android.support.AndroidSupportInjection
 import dog.snow.androidrecruittest.R
 import dog.snow.androidrecruittest.databinding.FragmentListBinding
 import javax.inject.Inject
 
 class ListFragment : Fragment() {
-
     @Inject
     lateinit var listViewModel: ListViewModel
     private lateinit var binding: FragmentListBinding
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
