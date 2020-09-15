e315a3b36403503d7f47e5a95e384dfb6107e171
Oskar Rek <oskarrek98@gmail.com>
2020-09-15 14:41:38 +0200

    feat: shaw connection banner

--- app/src/main/java/dog/snow/androidrecruittest/ui/common/broadcast/NetworkChangeReceiver.kt
+++ app/src/main/java/dog/snow/androidrecruittest/ui/common/broadcast/NetworkChangeReceiver.kt
@@ -4,16 +4,16 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import dagger.android.AndroidInjection
 
 
-class NetworkChangeReceiver(private val onChanged: () -> Unit) : BroadcastReceiver() {
+
+class NetworkChangeReceiver(
+    private val onChanged: (Boolean) -> Unit
+) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        AndroidInjection.inject(this, context)
         val connMgr = context
             .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if(connMgr.activeNetwork == null)
-            onChanged.invoke()
+            onChanged.invoke(connMgr.activeNetwork == null)
     }
 }
